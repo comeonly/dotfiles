57ed65162274b7d4ef61651166757eb91bad38d9
comeonly <atunori.kamori@gmail.com>
2014-02-15 21:05:09 +0900

    インストール時の表示を変更

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
+echo "====================================="
 echo "get comeonly's dotfiles"
+echo "====================================="
 git clone https://github.com/comeonly/dotfiles ~/dotfiles
+echo "====================================="
 echo "get submodeles"
+echo "====================================="
 cd ~/dotfiles
 git submodule update --init --recursive
+echo "====================================="
 echo "install oh-my-zsh"
+echo "====================================="
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo "====================================="
 echo "setup tmux"
+echo "====================================="
 ln -s ~/dotfiles/.tmux ~/.tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 rm -f ~/.tmux/tmux-powerline/theme/default.sh
 ln -s ~/dotfiles/.tmux/tmux-powerline-themes/default.sh ~/.tmux/tmux-powerline/themes/default.sh
 ~/.tmux/tmux-powerline/generate_rc.sh
 mv ~/.tmux-powerlinerc.default ~/.tmux-powerlinerc
+echo "====================================="
 echo "install janus"
+echo "====================================="
 curl -Lo- https://raw.github.com/carlhuda/janus/master/bootstrap.sh | bash
 ln -s ~/dotfiles/.janus ~/.janus
 ln -s ~/dotfiles/.vimrc.before ~/.vimrc.before
--- update.sh
+++ update.sh
@@ -2,7 +2,5 @@ upgrade_oh_my_zsh
 cd ~/dotfiles
 git pull --rebase
 git submodule foreach 'git checkout master; git pull'
-cd ~/.tmux/tmux-powerline
-git pull --rebase
 cd ~/.vim
 rake update
